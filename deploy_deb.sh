67d7fe463c7d49a1fa4faf4ed215e549ba351ba5
Volodymyr Pohyla <chromelviv@gmail.com>
2018-07-11 12:15:14 +0300

    Update deploy_deb.sh

--- deploy_deb.sh
+++ deploy_deb.sh
@@ -1,6 +1,10 @@
 
 #!/bin/bash
 
+echo "Create deb package..."
+
+fpm -s dir -t deb -C /home/vpokhylo/digibits/digitalbits-core/src --name digitalbits-core --version 0.1.0 --iteration 1 --depends debian_dependency1 --description "Digitalbits-core" .
+
 echo "deploying to Cloudsmith with cloudsmith-cli"
 
 cd src/
