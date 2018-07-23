e518f3f20a035d92fe86206e1a498dae7a713cf5
Volodymyr Pohyla <chromelviv@gmail.com>
2018-07-23 21:54:10 +0300

    Update build_deploy.sh

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
-fpm --version
+
+mkdir -p /usr/local/digitalbits/src \
+    && cat /root/.ssh/known_hosts \
+    && git clone --depth 1 --branch master git@github.com:DigitalBitsOrg/stellar-core.git /usr/local/digitalbits/src \
+    && cd /usr/local/digitalbits/src \
+    && git submodule init \
+    && git submodule update \
+    && ./autogen.sh \
+    && ./configure \
+    && make -j 6 \
+    && mkdir -p /tmp/build
+    && make DESTDIR=/tmp/build install
+
 echo "Create deb package..."
 
-#fpm -s dir -t deb -C src --name digitalbits-core --version 0.1.0 --iteration 1 --depends debian_dependency1 --description "Digitalbits-core" .
+fpm -s dir -t deb -C src --name digitalbits-core --version 0.1.0 --iteration 1 --depends debian_dependency1 --description "Digitalbits-core" .
 
 echo "Create rpm package..."
 
-#fpm -s dir -t rpm -C src --name digitalbits-core --version 0.1.0 --iteration 1 --depends  redhat_dependency1 --description "digitalbits-core" .
+fpm -s dir -t rpm -C src --name digitalbits-core --version 0.1.0 --iteration 1 --depends  redhat_dependency1 --description "digitalbits-core" .
 
 echo "deploying to Cloudsmith with cloudsmith-cli"
 
